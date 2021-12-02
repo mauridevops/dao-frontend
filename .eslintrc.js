549683d6f5deb926f353abae599f699e8773ee21
mauridevops <89289165+mauridevops@users.noreply.github.com>
2021-12-02 15:02:47 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,166 @@
-module.exports = {
-  env: {
-    browser: true,
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
+import { useState } from "react";
+import { addresses, TOKEN_DECIMALS } from "../../constants";
+import { NavLink } from "react-router-dom";
+import { Link, SvgIcon, Popper, Button, Paper, Typography, Divider, Box, Fade, Slide } from "@material-ui/core";
+import { ReactComponent as InfoIcon } from "../../assets/icons/info-fill.svg";
+import { ReactComponent as ArrowUpIcon } from "../../assets/icons/arrow-up.svg";
+import { ReactComponent as sOhmTokenImg } from "../../assets/tokens/token_sOHM.svg";
+import { ReactComponent as wsOhmTokenImg } from "../../assets/tokens/token_wsOHM.svg";
+import { ReactComponent as ohmTokenImg } from "../../assets/tokens/token_OHM.svg";
+import { ReactComponent as t33TokenImg } from "../../assets/tokens/token_33T.svg";
+import "./ohmmenu.scss";
+import { dai, frax } from "src/helpers/AllBonds";
+import { Trans } from "@lingui/macro";
+import Grid from "@material-ui/core/Grid";
+import OhmImg from "src/assets/tokens/token_OHM.svg";
+import SOhmImg from "src/assets/tokens/token_sOHM.svg";
+import WsOhmImg from "src/assets/tokens/token_wsOHM.svg";
+import token33tImg from "src/assets/tokens/token_33T.svg";
+import { segmentUA } from "../../helpers/userAnalyticHelpers";
+import { useSelector } from "react-redux";
+import { useWeb3Context } from "../../hooks";
+
+const addTokenToWallet = (tokenSymbol, tokenAddress, address) => async () => {
+  if (window.ethereum) {
+    const host = window.location.origin;
+    let tokenPath;
+    let tokenDecimals = TOKEN_DECIMALS;
+    switch (tokenSymbol) {
+      case "LHM":
+        tokenPath = OhmImg;
+        break;
+      case "sLHM":
+        tokenPath = WsOhmImg;
+        tokenDecimals = 18;
+        break;
+      default:
+        tokenPath = SOhmImg;
+    }
+    const imageURL = `${host}/${tokenPath}`;
+
+    try {
+      await window.ethereum.request({
+        method: "wallet_watchAsset",
+        params: {
+          type: "ERC20",
+          options: {
+            address: tokenAddress,
+            symbol: tokenSymbol,
+            decimals: tokenDecimals,
+            image: imageURL,
+          },
+        },
+      });
+      let uaData = {
+        address: address,
+        type: "Add Token",
+        tokenName: tokenSymbol,
+      };
+      segmentUA(uaData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
+
+function OhmMenu() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isEthereumAPIAvailable = window.ethereum;
+  const { address } = useWeb3Context();
+  const networkId = useSelector(state => state.network.networkId);
+
+  const SOHM_ADDRESS = addresses[networkId] && addresses[networkId].SOHM_ADDRESS;
+  const OHM_ADDRESS = addresses[networkId] && addresses[networkId].OHM_ADDRESS;
+  const PT_TOKEN_ADDRESS = addresses[networkId] && addresses[networkId].PT_TOKEN_ADDRESS;
+  const GOHM_ADDRESS = addresses[networkId] && addresses[networkId].GOHM_ADDRESS;
+
+  const handleClick = event => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = "ohm-popper";
+  const daiAddress = dai.getAddressForReserve(networkId);
+  const fraxAddress = frax.getAddressForReserve(networkId);
+  return (
+    <Grid
+      container
+      component="div"
+      onMouseEnter={e => handleClick(e)}
+      onMouseLeave={e => handleClick(e)}
+      id="ohm-menu-button-hover"
+    >
+      <Button id="ohm-menu-button" size="large" variant="contained" color="secondary" title="OHM" aria-describedby={id}>
+        <SvgIcon component={InfoIcon} color="primary" />
+        <Typography className="ohm-menu-button-text">LHM menu</Typography>
+      </Button>
+
+      <Popper id={id} open={open} anchorEl={anchorEl} placement="bottom-start" transition>
+        {({ TransitionProps }) => {
+          return (
+            <Fade {...TransitionProps} timeout={100}>
+              <Paper className="ohm-menu" elevation={1}>
+                <Box component="div" className="buy-tokens">
+                  <Link
+                    href={`https://app.sushi.com/swap?inputCurrency=${daiAddress}&outputCurrency=${OHM_ADDRESS}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Button size="large" variant="contained" color="secondary" fullWidth>
+                      <Typography align="left">
+                        <Trans>Buy on {new String("Pancakeswap")}</Trans>
+                        <SvgIcon component={ArrowUpIcon} htmlColor="#A3A3A3" />
+                      </Typography>
+                    </Button>
+                  </Link>
+                </Box>
+
+                {isEthereumAPIAvailable ? (
+                  <Box className="add-tokens">
+                    <Divider color="secondary" />
+                    <p>
+                      <Trans>ADD TOKENS TO WALLET</Trans>
+                    </p>
+                    <Box display="flex" flexDirection="row" justifyContent="space-between">
+                      {OHM_ADDRESS && (
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          onClick={addTokenToWallet("LHM", OHM_ADDRESS, address)}
+                        >
+                          <SvgIcon
+                            component={ohmTokenImg}
+                            viewBox="0 0 32 32"
+                            style={{ height: "25px", width: "25px" }}
+                          />
+                          <Typography variant="body1">LHM</Typography>
+                        </Button>
+                      )}
+                      {SOHM_ADDRESS && (
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          onClick={addTokenToWallet("sLHM", SOHM_ADDRESS, address)}
+                        >
+                          <SvgIcon
+                            component={sOhmTokenImg}
+                            viewBox="0 0 100 100"
+                            style={{ height: "25px", width: "25px" }}
+                          />
+                          <Typography variant="body1">sLHM</Typography>
+                        </Button>
+                      )}
+
+                    </Box>
+                  </Box>
+                ) : null}
+              </Paper>
+            </Fade>
+          );
+        }}
+      </Popper>
+    </Grid>
+  );
+}
+
+export default OhmMenu;
