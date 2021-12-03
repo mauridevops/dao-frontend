66770d46f4c7837ee6f2fab365b929149c96773e
mauridevops <contacto@mauriciooyola.com>
2021-12-03 19:34:11 -0300

    brand

--- src/components/Sidebar/NavContent.jsx
+++ src/components/Sidebar/NavContent.jsx
@@ -52,7 +52,7 @@ function NavContent() {
       <Box className="dapp-sidebar-inner" display="flex" justifyContent="space-between" flexDirection="column">
         <div className="dapp-menu-top">
           <Box className="branding-header">
-            <Link href="https://olympusdao.finance" target="_blank">
+            <Link href="https://aliwendao.finance" target="_blank">
               <SvgIcon
                 color="primary"
                 component={OlympusIcon}
