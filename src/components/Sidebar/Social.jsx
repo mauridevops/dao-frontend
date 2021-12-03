1f8a1c5922c17eca4dfa83b7ca55ab91bfcce3ce
mauridevops <contacto@mauriciooyola.com>
2021-12-03 02:21:28 -0300

    adjust

--- src/components/Sidebar/Social.jsx
+++ src/components/Sidebar/Social.jsx
@@ -1,5 +1,4 @@
 import { SvgIcon, Link } from "@material-ui/core";
-import { ReactComponent as GitHub } from "../../assets/icons/github.svg";
 import { ReactComponent as Medium } from "../../assets/icons/medium.svg";
 import { ReactComponent as Twitter } from "../../assets/icons/twitter.svg";
 import { ReactComponent as Discord } from "../../assets/icons/discord.svg";
@@ -7,9 +6,6 @@ import { ReactComponent as Discord } from "../../assets/icons/discord.svg";
 export default function Social() {
   return (
     <div className="social-row">
-      <Link href="https://github.com/OlympusDAO" target="_blank">
-        <SvgIcon color="primary" component={GitHub} />
-      </Link>
 
       <Link href="https://olympusdao.medium.com/" target="_blank">
         <SvgIcon color="primary" component={Medium} />
